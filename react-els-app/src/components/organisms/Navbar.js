d112560b2de504cadbb6464b9b35befb938cd070
jescabillas <jhonrhon.escabillas@sun-asterisk.com>
2022-11-10 15:38:41 +0800

    User authentication fixes

--- react-els-app/src/components/organisms/Navbar.js
+++ react-els-app/src/components/organisms/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import ProtectedTags from '../templates/ProtectedTags';
 
--- react-els-app/src/redux/store.js
+++ react-els-app/src/redux/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { 
     persistStore,
     persistReducer,
