ef0f7ab37392212d3a2e509b4176add52d479181
jescabillas <jhonrhon.escabillas@sun-asterisk.com>
2022-12-01 09:58:09 +0800

    [SELS-FEATURE] Words learned

--- els-api/app/Http/Controllers/ResultsController.php
+++ els-api/app/Http/Controllers/ResultsController.php
@@ -6,7 +6,8 @@ use App\Models\Answers;
 use App\Models\Choices;
 use App\Models\Lessons;
 use App\Models\Results;
-use Illuminate\Console\View\Components\Choice;
+use App\Models\User;
+use App\Models\Words;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -28,5 +29,25 @@ class ResultsController extends Controller
 
     public function completed() {
         return Results::where('users_id', '=', auth()->user()->id)->get();
+    }
+
+    public function words($id) {
+        $user = User::findOrFail($id);
+        $answers = Answers::where('users_id', '=', $user->id)->get();
+        $learned = [];
+        $words = [];
+
+        foreach($answers as $answer) {
+            $choice = Choices::where('id','=', $answer->choices_id)->firstOrFail();
+            if($choice->answer) {
+                array_push($learned, $choice);
+                array_push($words, Words::where('id','=', $choice->words_id)->firstOrFail());
+            }
+        }
+
+        return response()->json([
+            'answers'=> collect($learned)->paginate(10),
+            'words' => $words,
+        ]);
     }
 }
