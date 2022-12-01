a08aedb06c1288bd98f40c2d94c7c26a8bdf55d4
jescabillas <jhonrhon.escabillas@sun-asterisk.com>
2022-12-01 09:58:09 +0800

    [SELS-FEATURE] Words learned

---
+++ react-els-app/src/redux/wordsLearned.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const wordsLearnedSlice = createSlice({
+    name: 'wordsLearned',
+    initialState: {
+        wordsLearnedData: {
+            words: {
+                id: '',
+                word: ''
+            },
+            answers: {
+                id: '',
+                words_id: '',
+                choice: ''
+            }
+        },
+        wordsLearnedError: {
+            header: ''
+        }
+    },
+    reducers: {
+        setWordsLearnedData: (state, action) => {
+            state.wordsLearnedError = {};
+            state.wordsLearnedData[action.payload.key] = action.payload.value;
+        },
+        setWordsLearnedError: (state, action) => {
+            state.wordsLearnedError = {...state.wordsLearnedError, [action.payload.key]: action.payload.value};
+        }
+    }
+});
+
+export const { setWordsLearnedData, setWordsLearnedError } = wordsLearnedSlice.actions;
+
+export default wordsLearnedSlice.reducer;
